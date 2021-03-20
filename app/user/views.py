5ecb02afc66c9e4965792d7ed93dbce505287063
Ravindra Gadiparthi <Ravindra.Gadiparthi@vcontractor.co.za>
2021-03-20 16:43:31 +0530

    add manage endpoint

--- app/user/views.py
+++ app/user/views.py
@@ -1,16 +1,30 @@
-from django.shortcuts import render
 from django.contrib.auth import get_user_model
 from rest_framework.generics import CreateAPIView
-from .serializers import UserSerializer,AuthTokenSerializer
+from .serializers import UserSerializer, AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import RetrieveUpdateAPIView
 
 
 class UserCreateView(CreateAPIView):
-    name = 'create'
     """user mode view set"""
+    name = 'create'
     serializer_class = UserSerializer
     queryset = get_user_model().objects.all()
+
+
+class ManageUserView(RetrieveUpdateAPIView):
+    name = "manage"
+    serializer_class = UserSerializer
+    queryset = get_user_model().objects.all()
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+
+    def get_object(self):
+        """retrieve authenticated user"""
+        return self.request.user
 
 
 class UserTokenView(ObtainAuthToken):
